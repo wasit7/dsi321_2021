e4d56bf56f1cbad4ca5c25fcaed279b1e09ece22
Wasit Limprasert <wasit7@gmail.com>
2021-03-08 15:41:30 +0700

    change the name

--- composetest/app.py
+++ composetest/app.py
@@ -19,4 +19,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello Wasit! I have been seen {} times.\n'.format(count)
